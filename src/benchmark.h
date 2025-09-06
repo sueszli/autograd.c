676c20ae43fd71054620aed3488bc6caf042633e
sueszli <jabaryyahya@gmail.com>
2025-09-06 05:04:43 +0200

    up

---
+++ src/benchmark.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <time.h>
+#include <stdio.h>
+
+// clang-format off
+#define benchmark(name, block) \
+    do { \
+        struct timespec start, end; \
+        clock_gettime(CLOCK_MONOTONIC, &start); \
+        block; \
+        clock_gettime(CLOCK_MONOTONIC, &end); \
+        double time_spent = (double)(end.tv_sec - start.tv_sec) + (double)(end.tv_nsec - start.tv_nsec) / 1e9; \
+        printf(name " took %.3f seconds to execute\n", time_spent); \
+    } while (0)
+// clang-format on
--- src/main.c
+++ src/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+#include "benchmark.h"
 
 int main(void) {
     printf("Hello, World!\n");
+
+    benchmark("sleep benchmark", { sleep(4); });
+
     return EXIT_SUCCESS;
 }
