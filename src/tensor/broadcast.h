4c5fe30280fd107463ce95ac4a7d06a9443beab7
sueszli <jabaryyahya@gmail.com>
2025-09-09 22:53:58 +0200

    up

--- src/tensor/broadcast.h
+++ src/tensor/broadcast.h
@@ -35,7 +35,3 @@ bool tensor_can_broadcast(const Tensor *a, const Tensor *b);
 i32 *get_tensor_broadcast_shape(const Tensor *a, const Tensor *b, i32 *result_ndim);
 
 Tensor *tensor_broadcast_to(const Tensor *tensor, const i32 *target_shape, i32 target_ndim);
-
-Tensor *tensor_add_broadcast(Tensor *a, Tensor *b);
-
-Tensor *tensor_mul_broadcast(Tensor *a, Tensor *b);
