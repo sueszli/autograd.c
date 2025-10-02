5f3d6087f55ce59fea1fafa294342be1647b6586
sueszli <jabaryyahya@gmail.com>
2025-10-02 19:32:05 +0200

    up

--- src/main.c
+++ src/main.c
@@ -3,22 +3,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-static test_samples_t test_samples;
-static train_samples_t train_samples;
+extern sample_t test_samples[NUM_TEST_SAMPLES];
+extern sample_t train_samples[NUM_TRAIN_SAMPLES];
 
 int32_t main(void) {
-    load_test_samples(test_samples);
-
-    printf("Test samples:\n");
-    for (int8_t i = 0; i < 10; i++) {
-        printf("\tSample %d: %s\n", i, label_to_str(test_samples[i].label));
+    for (int8_t i = 0; i < 5; i++) {
+        printf("test sample %d: %s\n", i, label_to_str(test_samples[i].label));
     }
 
-    load_train_samples(train_samples);
-
-    printf("\nTrain samples:\n");
-    for (int8_t i = 0; i < 10; i++) {
-        printf("\tSample %d: %s\n", i, label_to_str(train_samples[i].label));
+    for (int8_t i = 0; i < 5; i++) {
+        printf("train sample %d: %s\n", i, label_to_str(train_samples[i].label));
     }
 
     return EXIT_SUCCESS;
