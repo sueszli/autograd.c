2b26e90dd1063baf9794d2186cd391f0047570bb
sueszli <jabaryyahya@gmail.com>
2025-12-15 19:24:01 +0100

    up

--- src/tensor.h
+++ src/tensor.h
@@ -8,13 +8,13 @@ typedef float float32_t;
 typedef double float64_t;
 
 typedef struct Tensor {
-    float32_t *data;        // flat contiguous array, row-major
-    uint64_t *shape;        // array of dimension sizes
-    uint64_t *strides;      // array of elements to skip to get to next element in each dimension
-    uint64_t ndim;          // rank (ie. 1 for vector, 2 for matrix, etc.)
-    uint64_t size;          // total number of elements
-    bool requires_grad;     // whether to track operations for autograd
-    struct Tensor *grad;    // accumulated gradient (del loss / del tensor) during backprop
+    float32_t *data;     // flat contiguous array, row-major
+    uint64_t *shape;     // array of dimension sizes
+    uint64_t *strides;   // array of elements to skip to get to next element in each dimension
+    uint64_t ndim;       // rank (ie. 1 for vector, 2 for matrix, etc.)
+    uint64_t size;       // total number of elements
+    bool requires_grad;  // whether to track operations for autograd
+    struct Tensor *grad; // accumulated gradient (del loss / del tensor) during backprop
 } Tensor;
 
 // memory management
