5b21d1d96a718b8e68fca374b148e1d22050addd
sueszli <jabaryyahya@gmail.com>
2025-12-15 16:52:42 +0100

    up

--- src/ops/activations.c
+++ src/ops/activations.c
@@ -4,22 +4,6 @@
 #include <math.h>
 #include <stddef.h>
 
-static inline float32_t sigmoid(float32_t x) {
-    if (x > 500.0f) {
-        x = 500.0f;
-    }
-    if (x < -500.0f) {
-        x = -500.0f;
-    }
-
-    if (x >= 0.0f) {
-        return 1.0f / (1.0f + expf(-x));
-    } else {
-        float32_t ex = expf(x);
-        return ex / (1.0f + ex);
-    }
-}
-
 Tensor *tensor_sigmoid(const Tensor *t) {
     assert(t != NULL);
     assert(t->data != NULL || t->size == 0);
@@ -27,8 +11,23 @@ Tensor *tensor_sigmoid(const Tensor *t) {
     Tensor *out = tensor_create(NULL, t->shape, t->ndim, t->requires_grad);
 
     for (uint64_t i = 0; i < t->size; i++) {
-        out->data[i] = sigmoid(t->data[i]);
+        float32_t x = t->data[i];
+        if (x > 500.0f) {
+            x = 500.0f;
+        }
+        if (x < -500.0f) {
+            x = -500.0f;
+        }
+
+        if (x >= 0.0f) {
+            out->data[i] = 1.0f / (1.0f + expf(-x));
+        } else {
+            float32_t ex = expf(x);
+            out->data[i] = ex / (1.0f + ex);
+        }
     }
+
+    // TODO: FIX THIS
     if (out->requires_grad) {
         out->grad_fn = new_sigmoid_backward((Tensor *)t, out);
         out->grad_fn->out_tensor = out;
@@ -46,6 +45,7 @@ Tensor *tensor_relu(const Tensor *t) {
         float32_t x = t->data[i];
         out->data[i] = (x > 0.0f) ? x : 0.0f;
     }
+    // TODO: FIX THIS
     if (out->requires_grad) {
         out->grad_fn = new_relu_backward((Tensor *)t);
         out->grad_fn->out_tensor = out;
@@ -62,6 +62,7 @@ Tensor *tensor_tanh(const Tensor *t) {
     for (uint64_t i = 0; i < t->size; i++) {
         out->data[i] = tanhf(t->data[i]);
     }
+    // TODO: FIX THIS
     return out;
 }
 
@@ -76,6 +77,7 @@ Tensor *tensor_gelu(const Tensor *t) {
         out->data[i] = 0.5f * x * (1.0f + erff(x * 1 / (float)sqrt(2)));
     }
 
+    // TODO: FIX THIS
     if (out->requires_grad) {
         out->grad_fn = new_gelu_backward((Tensor *)t);
         out->grad_fn->out_tensor = out;
@@ -101,6 +103,7 @@ Tensor *tensor_softmax(const Tensor *t, int64_t dim) {
     tensor_free(shifted);
     tensor_free(sum_exp);
 
+    // TODO: FIX THIS
     if (out->requires_grad) {
         out->grad_fn = new_softmax_backward((Tensor *)t, out, dim);
         out->grad_fn->out_tensor = out;
