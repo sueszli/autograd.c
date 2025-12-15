33db1f44f1bb879f28033a8a9378e8a809b0300a
sueszli <jabaryyahya@gmail.com>
2025-12-15 16:56:07 +0100

    up

--- src/ops/activations.h
+++ src/ops/activations.h
@@ -2,6 +2,7 @@
 
 #include "tensor.h"
 
+// can't be named just `tanh` due to conflict with standard library
 Tensor *tensor_sigmoid(const Tensor *t);
 Tensor *tensor_relu(const Tensor *t);
 Tensor *tensor_tanh(const Tensor *t);
